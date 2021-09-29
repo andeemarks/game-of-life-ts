a10dc71bd555728f243d062b245d02313d865365
Andy Marks <vampwillow@gmail.com>
2021-09-29 14:40:57 +1000

    Cloning of generations

--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -49,10 +49,10 @@ function showBoard(cells: Cell[][]) {
 }
 
 function updateBoard() {
-  console.log("updating board...");
-  let next: Generation = new Generation(10, 10);
-  for (let x = 0; x < 10; x++) {
-    for (let y = 0; y < 10; y++) {
+  console.log("Generation: " + current.id());
+  let next = current.template();
+  for (let x = 0; x < current.width; x++) {
+    for (let y = 0; y < current.height; y++) {
       next.update(x, y, current.evolve(x, y));
     }
   }
