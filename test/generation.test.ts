ecb0f4917cd3df8cdf6214899eee0c1144896a9f
Andy Marks <vampwillow@gmail.com>
2021-09-29 10:52:29 +1000

    + Generation for holding cells

--- test/generation.test.ts
+++ test/generation.test.ts
@@ -2,9 +2,11 @@ import Generation from "../src/generation";
 import Cell from "../src/cell";
 
 describe("Generation", () => {
-  it("holds all cells for specified dimensions", () => {
+  it("defaults to 0 size", () => {
     expect(new Generation().cells().length).toBe(0);
-    expect(new Generation(1, 1).cells().length).toBe(1);
+  });
+
+  it("holds all cells for specified dimensions", () => {
     let cells = new Generation(2, 3).cells();
     expect(cells.length).toBe(2);
     expect(cells[0].length).toBe(3);
@@ -15,5 +17,10 @@ describe("Generation", () => {
     expect(cells[1][0]).toBeInstanceOf(Cell);
     expect(cells[1][1]).toBeInstanceOf(Cell);
     expect(cells[1][2]).toBeInstanceOf(Cell);
+  });
+
+  it("finds a full set of neighbours for a cell away from borders", () => {
+    let neighbours = new Generation(10, 10).neighbours(5, 5);
+    expect(neighbours.length).toBe(8);
   });
 });
