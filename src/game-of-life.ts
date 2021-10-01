694c90820358b58536f6cb8dd3012fb54967555d
Andy Marks <vampwillow@gmail.com>
2021-10-02 08:44:45 +1000

    Basic command line usage and options

--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -2,6 +2,7 @@
 import { Generation, EntropyError } from "./generation";
 import Board from "./board";
 import commandLineArgs from "command-line-args";
+import commandLineUsage from "command-line-usage";
 
 function seed(gen: Generation): Generation {
   gen.seed(4, 5);
@@ -15,28 +16,86 @@ function seed(gen: Generation): Generation {
 
 async function gameLoop(): Promise<void> {
   const optionDefinitions = [
-    { name: "verbose", alias: "v", type: Boolean },
+    { name: "help", alias: "h", type: Boolean },
     { name: "width", type: Number, multiple: false, defaultValue: 20 },
     { name: "height", type: Number, multiple: false, defaultValue: 20 },
     { name: "delay", type: Number, defaultValue: 250 },
   ];
 
+  const sections = [
+    {
+      header: "Game of Life",
+      content: "Terminal version of {bold Conway's Game of Life} simulation.",
+    },
+    {
+      header: "Options",
+      optionList: [
+        {
+          name: "width",
+          typeLabel: "{underline number}",
+          type: Number,
+          description: "Width of simulation in characters.  Defaults to 20.",
+        },
+        {
+          name: "height",
+          typeLabel: "{underline number}",
+          type: Number,
+          description: "Height of simulation in characters.  Defaults to 20.",
+        },
+        {
+          name: "delay",
+          typeLabel: "{underline ms}",
+          type: Number,
+          description: "Time in millis between evolutions.  Defaults to 250.",
+        },
+        {
+          name: "help",
+          alias: "h",
+          type: Boolean,
+          description: "Print this usage guide.",
+        },
+      ],
+    },
+    {
+      header: "Examples",
+      content: [
+        {
+          desc: "1. All defaults (20x20 with 250ms delay). ",
+          example: "$ yarn game",
+        },
+        {
+          desc: "2. A slower evolution. ",
+          example: "$ yarn game --delay 1000",
+        },
+        {
+          desc: "3. A bigger simulation ",
+          example: "$ yarn game --width 25 --height 40",
+        },
+      ],
+    },
+  ];
+
   const options = commandLineArgs(optionDefinitions);
+  const usage = commandLineUsage(sections);
 
-  let current: Generation = new Generation(options.width, options.height);
-  current = seed(current);
+  if (options.help) {
+    console.log(usage);
+  } else {
+    let current: Generation = new Generation(options.width, options.height);
+    current = seed(current);
 
-  const board: Board = new Board();
-  board.show(current);
+    const board: Board = new Board();
+    board.show(current);
 
-  let isEvolving = true;
-  while (isEvolving) {
-    try {
-      current = current.regenerate();
-      board.show(current);
-      await delay(options.delay);
-    } catch (error) {
-      if (error instanceof EntropyError) isEvolving = false;
+    let isEvolving = true;
+    while (isEvolving) {
+      try {
+        current = current.regenerate();
+        board.show(current);
+        await delay(options.delay);
+      } catch (error) {
+        if (error instanceof EntropyError) isEvolving = false;
+      }
     }
   }
 }
